0094ab96a2afa09c8de5cba09c2258b4aee9f7d3
Ryan McGivern <github@ryanmcgivern.com>
2017-04-04 11:41:56 -0500

    Test Branch 2

--- Concept Calculator/Concept Calculator/Form1.Designer.cs
+++ Concept Calculator/Concept Calculator/Form1.Designer.cs
@@ -56,9 +56,9 @@
             this.lblOutput.AutoSize = true;
             this.lblOutput.Location = new System.Drawing.Point(173, 21);
             this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(13, 13);
+            this.lblOutput.Size = new System.Drawing.Size(26, 13);
             this.lblOutput.TabIndex = 0;
-            this.lblOutput.Text = "0";
+            this.lblOutput.Text = "Bob";
             // 
             // btn7
             // 
